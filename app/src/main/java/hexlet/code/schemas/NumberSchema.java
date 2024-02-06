246ee5777c325d3a65b91c7bf1486aa4b56638e2
vmanannikov <manhetan@gmail.com>
2024-02-06 22:03:06 +0300

    fix NumberSchema, create main.yaml

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -8,12 +8,12 @@ public class NumberSchema extends BaseSchema<Number> {
     }
 
     public NumberSchema positive() {
-        super.addCheck(value -> value instanceof Integer && (Integer) value > 0);
+        super.addCheck(value -> (Integer) value > 0);
         return this;
     }
 
     public NumberSchema range(int min, int max) {
-        super.addCheck(value -> value instanceof Integer && (Integer) value >= min && (Integer) value <= max);
+        super.addCheck(value -> (Integer) value >= min && (Integer) value <= max);
         return this;
     }
 }
